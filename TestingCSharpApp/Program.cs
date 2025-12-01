3c5d8ae7bf6e6f789c7cb7f3a5b8622470f4c7a7
Arnauw <arnaud.rabel@gmail.com>
2025-12-01 17:47:00 +0100

    first commit, trying to make a calculator desktop app in csharp using avalonia

---
+++ TestingCSharpApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Hello, what's your name: ");
+string name = Console.ReadLine() ?? "";
+
+Console.WriteLine();
+
+// Test comment
+
+/*
+ * Multiline comment test.
+ */
+if (string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("You didn't enter a name... you fucking retard!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
